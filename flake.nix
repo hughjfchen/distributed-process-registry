c0dec274d06e868eede41a953e1622f7309e9ec2
Hugh JF Chen <hugh.jf.chen@gmail.com>
2025-05-14 10:57:47 +0800

    nixify it

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "The extended registry for Cloud Haskell";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            distributed-process-registryProject =
+              final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc967";
+                # This is used by `nix develop .` to open a shell for use with
+                # `cabal`, `hlint` and `haskell-language-server`
+                shell.tools = {
+                  cabal = { };
+                  hlint = { };
+                  haskell-language-server = { };
+                };
+                # Non-Haskell shell tools go here
+                shell.buildInputs = with pkgs; [
+                  nixpkgs-fmt
+                ];
+                # This adds `js-unknown-ghcjs-cabal` to the shell.
+                # shell.crossPlatforms = p: [p.ghcjs];
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.distributed-process-registryProject.flake {
+          # This adds support for `nix build .#js-unknown-ghcjs:hello:exe:hello`
+          crossPlatforms = p: if (system == "x86_64-linux") then [ p.musl64 ] else [ ];
+        };
+      in
+      pkgs.lib.recursiveUpdate flake { packages.default = flake.packages."distributed-process-registry:lib:distributed-process-registry"; });
+}
